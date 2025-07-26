09df18fed3917d6976847e99d1137fbf99087026
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-07-26 15:33:52 +0200

    Add product entity class to project

--- src/main/java/com/market/digital_market/model/product/Product.java
+++ src/main/java/com/market/digital_market/model/product/Product.java
@@ -1,4 +1,78 @@
 package com.market.digital_market.model.product;
 
+import com.market.digital_market.model.business.Business;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "product")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")    private String description;
+    private Double price;
+
+    @OneToMany(mappedBy = "product",
+            fetch = FetchType.LAZY,
+            orphanRemoval = true,
+            cascade = CascadeType.ALL)
+    private List<Business> businesses;
+
+    public Product(Long id, String name, String description, Double price, List<Business> businesses) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.businesses = businesses;
+    }
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public List<Business> getBusinesses() {
+        return businesses;
+    }
+
+    public void setBusinesses(List<Business> businesses) {
+        this.businesses = businesses;
+    }
 }
---
+++ src/main/java/com/market/digital_market/model/product/ProductDTO.java
@@ -0,0 +1,12 @@
+package com.market.digital_market.model.product;
+
+import com.market.digital_market.model.business.Business;
+
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Business business;
+
+}
