7a6c094831ae5be1620e2eb50a77a93920d24dee
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-07-26 14:37:14 +0200

    Add business table to database

--- src/main/java/com/market/digital_market/model/Business.java
+++ src/main/java/com/market/digital_market/model/Business.java
@@ -1,4 +1,51 @@
 package com.market.digital_market.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "business")
 public class Business {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "department")
+    private String department;
+
+    public Business(Long id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    public Business() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
