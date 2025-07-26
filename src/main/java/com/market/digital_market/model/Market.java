b31c51acb26f17cac04acc1fbb6aecc608bc1914
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-07-26 14:43:11 +0200

    Add market table to database

--- src/main/java/com/market/digital_market/model/Market.java
+++ src/main/java/com/market/digital_market/model/Market.java
@@ -1,4 +1,44 @@
 package com.market.digital_market.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "market")
 public class Market {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "market",
+            fetch = FetchType.LAZY,
+            orphanRemoval = true,
+            cascade = CascadeType.ALL)
+    private List<Business> businesses;
+
+    public Market(Long id, List<Business> businesses) {
+        this.id = id;
+        this.businesses = businesses;
+    }
+
+    public Market() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Business> getBusinesses() {
+        return businesses;
+    }
+
+    public void setBusinesses(List<Business> businesses) {
+        this.businesses = businesses;
+    }
 }
